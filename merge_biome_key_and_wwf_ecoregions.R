55cd694e0ebb63f0ee35d25f05125c70aa2b9482
Daniel McGlinn <danmcglinn@gmail.com>
2014-02-28 12:18:48 -0700

    Drop the field "Fine_biome" from the merged file becuase it is identical to the field "Fine".

--- merge_biome_key_and_wwf_ecoregions.R
+++ merge_biome_key_and_wwf_ecoregions.R
@@ -5,4 +5,7 @@ wwf_eco = read.csv('wwf_eco_lookup.csv')
 
 wwf_biome_merge = merge(wwf_eco, biome_key, by='Code')
 
+## drop the Fine_biome field because this identical to the Fine field
+wwf_biome_merge = wwf_biome_merge[, -grep("Fine_biome", names(wwf_biome_merge))]
+
 write.csv(wwf_biome_merge, file='wwf_biome_merge.csv', row.names=F)
